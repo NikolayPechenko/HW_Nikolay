a14a0cd76a26e34dcdbaccc39c26bb8c4505fed9
Nikolay <k.pechenko1@gmailcom>
2024-05-21 14:42:59 +0300

    Add HW2.2

---
+++ Lessons/LS Module 2/While цикл.py
@@ -0,0 +1,24 @@
+# while 1 > 0:
+#     number = int(input('Введите число '))
+#     if number % 2 == 0:
+#         print('Число четное')
+#     else:
+#         print('Число нечетное')
+
+# while 1 > 0:
+#     number = int(input('Введите число '))
+#     if number % 2 == 0:
+#         print('Число четное')
+#     else:
+#         print('Число нечетное')
+#         break
+# print ('gg')
+
+# while 1 > 0:
+#     number = int(input('Введите число '))
+#     if number % 2 == 0:
+#         print('Число четное')
+#         continue
+#     else:
+#         print('Число нечетное')
+#     print('gg')
