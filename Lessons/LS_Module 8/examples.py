568ef20839514a3ccea50a8e5274c8b5a3176288
Nikolay <k.pechenko1@gmailcom>
2024-07-21 15:14:41 +0300

    add HW8.2

---
+++ Lessons/LS_Module 8/examples.py
@@ -0,0 +1,78 @@
+def f1(number):
+    return 10 / number
+
+
+def f2():
+    print('Какой хороший день!')  # порядок важен
+    result = f1(0)
+    return result
+
+
+try:
+    total = f2()
+    print(total)
+except ZeroDivisionError as exc:
+    print(f'Не работает - {exc}')
+
+
+def f1(number):
+    return 10 / number
+
+
+def f2():
+    print('Какой хороший день!')  # порядок важен
+    summ = 0
+    for i in range(-2, 2):
+        summ += f1(i)
+        print(summ)
+    return summ
+
+
+try:
+    total = f2()  # в Тотал ничего не сохранилось
+    print(total)
+except ZeroDivisionError as exc:
+    print(f'Не работает - {exc}')
+
+
+def f1(number):
+    return 10 / number
+
+
+def f2():
+    print('Какой хороший день!')  # порядок важен
+    summ = 0
+    for i in range(-2, 2):
+        summ += f1(i)
+        print(summ)
+    return summ
+
+
+try:
+    total = f2()  # в Тотал ничего не сохранилось
+    print(total)
+except ZeroDivisionError as exc:
+    print(f'Не работает - {exc}')
+
+
+def f1(number):
+    return 10 / number
+
+
+def f2():
+    print('Какой хороший день!')  # порядок важен
+    summ = 0
+    for i in range(-2, 2):
+        try:
+            summ += f1(i)
+            print(summ)
+        except ZeroDivisionError as exc:  # перехватили прямо здесь, продолжили работу функции
+            print(f'Что-то пошло не так - {exc}')
+    return summ 
+
+
+try:
+    total = f2()  # в Тотал ничего не сохранилось
+    print(total)
+except ZeroDivisionError as exc:
+    print(f'Не работает - {exc}')
