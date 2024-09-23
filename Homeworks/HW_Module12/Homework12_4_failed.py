c47ea4c58e750164bbe73381a27ede0ae588d8a4
Nikolay <k.pechenko1@gmailcom>
2024-09-23 20:06:20 +0300

    add HW12.4

---
+++ Homeworks/HW_Module12/Homework12_4_failed.py
@@ -0,0 +1,44 @@
+import logging
+import unittest
+import rt_with_exceptions
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            john = rt_with_exceptions.Runner('John', -50)
+            for i in range(10):
+                john.walk()
+            self.assertEqual(john.distance, 50)
+            logging.info(f'"test_walk" выполнен успешно')
+        except:
+            logging.warning(f'Неверная скорость для Runner', exc_info=True)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        try:
+            john = rt_with_exceptions.Runner(50)
+            for i in range(10):
+                john.run()
+            self.assertEqual(john.distance, 100)
+            logging.info(f'"test_run" выполнен успешно')
+        except:
+            logging.warning(f'Неверный тип данных для объекта Runner', exc_info=True)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        bob = rt_with_exceptions.Runner('Bob')
+        shisha = rt_with_exceptions.Runner('Shisha')
+        for i in range(10):
+            bob.walk()
+        for i in range(10):
+            shisha.run()
+        self.assertNotEqual(bob.distance, shisha.distance)
+
+
+if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', encoding='UTF-8',
+                        format='%(asctime)s | %(levelname)s |  %(message)s')
--- Lessons/LS_Module12/calc.py
+++ Lessons/LS_Module12/calc.py
@@ -1,3 +1,5 @@
+import logging
+
 def add(a, b):
     return a + b
 
@@ -11,11 +13,25 @@ def mul(a, b):
 
 
 def div(a, b):
-    return a / b
+    try:
+        a / b
+        logging.info(f'Podelili {a} na {b} s kaifom')
+        return a / b
+    except ZeroDivisionError as error:
+        logging.error(f'Gay!', exc_info=True)
+        return 0
 
 
 def sqrt(a):
-    return a**0.5
+    try:
+        if a > 0:
+            logging.info(f's kaifom izvlekli koreny iz {a}')
+            return a ** 0.5
+        else:
+            0 / 0
+    except:
+        logging.error(f'Gay!, otricatelnoe chislo', exc_info=False)
+        return 0
 
 
 def pow(a, b):
@@ -23,4 +39,15 @@ def pow(a, b):
 
 
 if __name__ == '__main__':
-    print(add(100, 3))
+    # logging.debug()
+    # logging.info()
+    # logging.warning()
+    # logging.error()
+    # logging.critical() - распределены по уровню важности (критичности)
+    logging.basicConfig(level=logging.INFO, filemode='w', filename='py.log',
+                        format='%(asctime)s | %(levelname)s |  %(message)s')
+
+
+print(__name__)
+
+
