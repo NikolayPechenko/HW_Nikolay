ac79270047606e0933408be70f66ec1e1aa4cb6a
Nikolay <k.pechenko1@gmailcom>
2024-10-03 18:06:00 +0300

    add HW13.3

--- Lessons/LS_Module13/aiogram1.py
+++ Lessons/LS_Module13/aiogram1.py
@@ -11,15 +11,17 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 @dp.message_handler(text=['Urban', 'ff'])
 async def urban_message(message):
     print('Urban сообщение')
-
+    await message.answer('Опачки')
 
 @dp.message_handler(commands='allo')
 async def start_messages(message):
     print('Стартовое сообщение')
+    await message.answer('У смартафона')
 
 @dp.message_handler()
 async def all_messages(message):
     print('Мы получили сообщение')
+    await message.answer(message.text.upper() + ' - ляляля'.upper())
 
 
 if __name__ == '__main__':
